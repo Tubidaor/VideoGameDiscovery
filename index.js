9c60a7d4603c9cb60106f1e1bc38513b2a6ed40e
Juan Baltazar <juan.baltazar1@gmail.com>
2019-09-14 12:45:09 -0700

    initial commit for Video game project

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const giantBombAPI = "5841ac31e4fdccc68c350d2280fc3fe3992c1d08"
+
+let date = new Date("12/05/2016");
+
+const gBUrl = `https://www.giantbomb.com/api/games/`
+
+const rawgUrl = `https://api.rawg.io/api/games?ordering=-added&dates=${formatDate(date)}%2C${formatDate(date)}`
+
+let gbUrlString = gBUrl + "?filter=original_release_date:" + formatDate(date) + "&format=json" + "&" + "api_key=" + giantBombAPI
+
+function formatDate(date) {
+  let month = (date.getMonth() + 1);
+  let day = date.getDate();
+  let year = date.getFullYear();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+    console.log(month.length);
+  return [year, month, day].join("-");
+}
+
+$(function displayDate() {
+  console.log(formatDate(date));
+  
+})
+
+function getRawg() {
+  fetch(rawgUrl)
+  .then(response => response.json())
+  .then(responseJson => console.log(responseJson));
+}
+
+const options = {
+  headers: new Headers({
+    "Allow-Control-Allow-Origin": 
+  })
+}
+function getGb() {
+  console.log(gbUrlString)
+  fetch(gbUrlString, options)
+  .then(response => response.json())
+  .then(responseJson => console.log(responseJson));
+}
+
+$(getRawg)
+$(getGb)
